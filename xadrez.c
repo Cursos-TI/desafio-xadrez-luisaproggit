42397e9a618730929cd9863b9102f1f81e7be208
luisaproggit <marialuisa.github@gmail.com>
2025-10-28 02:17:02 +0000

    alterado while

--- xadrez.c
+++ xadrez.c
@@ -4,47 +4,52 @@ int main() {
 
 //Mover a torre 5 casas para a direita.
 
-    for (int i = 1; i <= 5;  i++) {
-        printf("Direita\n"); // Imprimir a direção do movimento da Torre.
-        
+    for (int i = 0; i < 5;  i++) {
+        printf("Direita\n"); // Imprimir a direção do movimento da Torre.   
     }
 
 //Mover o Bispo 5 casas na diagonal para cima à direita.
     
-    int i = 1;
+    int i = 1; 
+
         while (i <=5) {
             printf("Cima,Direita\n"); // Imprimir a direção do movimento do Bispo.
             i++;
     }
 
 //Mover a Rainha 8 casas para a esquerda.
-
+        i = 1;
         do {
+
             printf("Esquerda\n"); // Imprime a direção do movimento da rainha.
+
             i++;
 
-        } while (i <= 8);      
+    }while (i <= 8);
+
+    
+        
+
 
 //Declarar variáveis para movimento cavalo.
 
-    int movimentos_para_baixo = 2;
-    int movimentos_para_esquerda = 1;
-    int j = 1;
-
-while (j = 0, j <= 1); 
+    int movimentoCompelto = 1; 
+    
+    while (movimentoCompelto--)
 
     {
-    for (int i = 0; i < 2; i++) 
-         printf("Baixo\n");//Imprime "Baixo" duas vezes.
-    }
+        for (int i = 0; i < 2; i++) {
+             printf("Baixo\n");//Imprime "Baixo" duas vezes.
+        }
 
     printf("Esquerda\n");//Imprime "Esquerda" uma vez.
     
+    }
 
-        return 0;
+    return 0;
         
-}
 
+}
 
 
 
